269d70c95b9bee94e3a5244ceaa7f0a5383f4d4a
Joshua Loyal <joshua.d.loyal@gmail.com>
2017-07-19 15:37:45 -0400

    remove target_size from mosaic_plot example

--- examples/mosaic_plot.py
+++ examples/mosaic_plot.py
@@ -8,5 +8,4 @@ import image_vis as ivs
 image_dir, data = ivs.load_dataset('rothko')
 
 with ivs.plotting_context(image_col='image_path', image_dir=image_dir):
-    ivs.mosaic_plot(data=data, sort_by=ivs.HSVFeatures.SATURATION, n_jobs=-1,
-                    target_size=(40, 40))
+    ivs.mosaic_plot(data=data, sort_by=ivs.HSVFeatures.SATURATION)
